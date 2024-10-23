6008c2bd24f494415a7502379c7ea9dbdcaee943
Saksham Mishra <140255588+SakshamMishra2023@users.noreply.github.com>
2024-10-23 19:42:37 +0530

    test commit2

--- core/src/main/java/Main_menu_screen/Level_selection.java
+++ core/src/main/java/Main_menu_screen/Level_selection.java
@@ -22,7 +22,7 @@ import angry.birds.game.Angry_ghosts;
 public class Level_selection implements Screen {
     private Angry_ghosts game;
     private OrthographicCamera camera;
-    private Viewport viewport;//gvgy
+    private Viewport viewport;//gvgygvhvh
     private Stage stage;
     private Skin skin;
     private Texture background;
